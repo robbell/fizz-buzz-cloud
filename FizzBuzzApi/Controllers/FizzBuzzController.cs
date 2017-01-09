cbd58573fd7d54b8e76d57df31f430dd3a8a0fab
Rob Bell <rob.bell@asos.com>
2017-01-09 13:54:12 +0000

    Add writing to queue from API

--- FizzBuzzApi/Controllers/FizzBuzzController.cs
+++ FizzBuzzApi/Controllers/FizzBuzzController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
+using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
 
 namespace FizzBuzzApi.Controllers
 {
@@ -6,6 +10,37 @@ namespace FizzBuzzApi.Controllers
     {
         public void Put(int number)
         {
+            WriteToQueue(number);
+        }
+
+        private void WriteToQueue(int number)
+        {
+            var connectionString = CloudConfigurationManager.GetSetting("ServiceBus.ConnectionString");
+            var queuePath = "FizzBuzzInput";
+
+            EnsureQueueExists(connectionString, queuePath);
+
+            SendMessage(connectionString, queuePath, number);
+        }
+
+        private static void EnsureQueueExists(string connectionString, string queuePath)
+        {
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.QueueExists(queuePath))
+            {
+                namespaceManager.CreateQueue(queuePath);
+//                namespaceManager.CreateTopic(new TopicDescription("") {  DefaultMessageTimeToLive = new TimeSpan(10) });
+            }
+        }
+
+        private static void SendMessage(string connectionString, string queuePath, int number)
+        {
+            var message = new BrokeredMessage(number);
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queuePath);
+
+            client.Send(message);
         }
     }
 }
